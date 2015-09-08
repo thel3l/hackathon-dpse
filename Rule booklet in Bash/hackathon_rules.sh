4957dddf2d1ea83546dee5dc692d2f656fa0e6d2
thel3l <i.rithwik@protonmail.com>
2015-09-08 10:21:33 +0000

    added menu options

--- Rule booklet in Bash/hackathon_rules.sh
+++ Rule booklet in Bash/hackathon_rules.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 NOW=$(date +"%m-%d-%y-%H%M%S")    #Date format
 ############### MAIN MENU ###############
+cat asciiArt.txt
 f_mainMenu(){
 clear
-echo""
-
+cat asciiArt.txt
+echo "[*] Enter '1' for items to be brought by you."
+echo "[*] Enter '2' to view the timetable for the event."
+echo "[*] Enter '3' to view the rules and guidelines for the event."
 }
