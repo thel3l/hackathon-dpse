5ec8898b141c70c7252c9296e0d32e231a61bd82
thel3l <i.rithwik@protonmail.com>
2015-09-11 21:17:49 +0530

    rules.c now accepts input 1

--- rules.c
+++ rules.c
@@ -29,11 +29,14 @@ void print_image(FILE *fptr)
     while(fgets(read_geniass,sizeof(read_geniass),fptr) != NULL)
         printf("%s",read_geniass);
       //bLEK.
+int option;
+      scanf("%d", &option);
+if ( option == 1) {
+      printf("\n1. You can have a maximum of 5 members in a team.");
+      printf("\n2. To ensure a level field for all contestants, all code, design, art, music and assets MUST be created during the duration of the Hackathon. \n   We want to ensure that all participants start off on the same footing and we also want \n   to preserve the true nature of a hackathon. The only exception to this rule would include material that is freely available to the public like\n");
+      printf("   Creative Commons Images and third party apis.");
+      printf("\n3. Winning submissions may be subject to code review.");
+      printf("\n   Be kind to others. Do not insult or put down other attendees. Behave professionally. Remember that harassment and racist, sexist, or exclusionary \n   jokes are not appropriate for this event.");
+      printf("\n   Hack, meet new people, get creative, and have a good time. Getting something meaningful done within such a short amount of time can be stressful, \n   but we'd like to encourage everyone to take a break every now and then to relax and enjoy the event.\n \r");    }
 
-    printf("\n1. You can have a maximum of 5 members in a team.");
-    printf("\n2. To ensure a level field for all contestants, all code, design, art, music and assets MUST be created during the duration of the Hackathon. \n   We want to ensure that all participants start off on the same footing and we also want \n   to preserve the true nature of a hackathon. The only exception to this rule would include material that is freely available to the public like\n");
-    printf("   Creative Commons Images and third party apis.");
-    printf("\n3. Winning submissions may be subject to code review.");
-    printf("\n   Be kind to others. Do not insult or put down other attendees. Behave professionally. Remember that harassment and racist, sexist, or exclusionary \n   jokes are not appropriate for this event.");
-    printf("\n   Hack, meet new people, get creative, and have a good time. Getting something meaningful done within such a short amount of time can be stressful, \n   but we'd like to encourage everyone to take a break every now and then to relax and enjoy the event.\n \r");
 }
