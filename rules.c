4e4393e38e1a645a8cdfac439eedd1ac6f64f94f
thel3l <i.rithwik@protonmail.com>
2015-09-15 21:55:11 +0530

    commented out unnecessary color codes

--- rules.c
+++ rules.c
@@ -1,12 +1,12 @@
 //by thel3l.
 // #include <conio.h>    //NOT portable. Need to find a way to clearscreen.
 #include <stdio.h>
-#define KNRM  "\x1B[0m"
-#define KRED  "\x1B[31m"
+// #define KNRM  "\x1B[0m"
+// #define KRED  "\x1B[31m"
 #define KGRN  "\x1B[32m"
-#define KYEL  "\x1B[33m"
-#define KBLU  "\x1B[34m"
-#define KMAG  "\x1B[35m"
+// #define KYEL  "\x1B[33m"
+// #define KBLU  "\x1B[34m"
+// #define KMAG  "\x1B[35m"
 #define KCYN  "\x1B[36m"
 #define KWHT  "\x1B[37m"
 #define RESET "\033[0m"
