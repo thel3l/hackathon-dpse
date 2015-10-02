4ce825dfc4a7acca24fc4bbb2647b0fa46d9d306
rithvikvibhu <rithvikvibhu@gmail.com>
2015-10-02 16:22:44 +0000

    Navbar stay on top
    Navbar stay on top

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -6,7 +6,7 @@ $(function() {
             if (target.length) {
                 $('html,body').animate({
                     scrollTop: target.offset().top
-                }, 1000);
+                }, 500);
                 return false;
             }
         }
