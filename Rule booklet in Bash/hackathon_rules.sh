7b1544504a0bea06ee3f8c7440b1aeefbefad0f7
thel3l <i.rithwik@protonmail.com>
2015-10-03 15:14:24 +0530

    added menu 2

--- Rule booklet in Bash/hackathon_rules.sh
+++ Rule booklet in Bash/hackathon_rules.sh
@@ -17,3 +17,21 @@ case $mainChoice in
 1) clear; f_option1;;
 2) clear; f_option2;;
 3) clear; f_option3;;
+*) clear; f_mainMenu;;
+esac
+}
+
+############### OPTION 1  ###############
+f_option1(){
+echo "[*] For the Hackathon, you will be expected to bring the following items:"
+echo "1. Laptops"
+echo "2. Smartphones"
+echo "3. Chargers"
+echo "4. Internet Dongle"
+echo "5. Ideas"
+echo "6. A backpack"
+echo "7. Headphones"
+echo "8. Extension board"
+echo "9. Notebook & Pen"
+echo "10. Flash Drives"
+}
