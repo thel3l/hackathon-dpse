33f0587dd2fa1fa66f3504870698398083b94c75
thel3l <i.rithwik@protonmail.com>
2015-10-03 15:02:45 +0530

    added vatious menu heads

--- Rule booklet in Bash/hackathon_rules.sh
+++ Rule booklet in Bash/hackathon_rules.sh
@@ -8,4 +8,12 @@ cat asciiArt.txt
 echo "[*] Enter '1' for items to be brought by you."
 echo "[*] Enter '2' to view the timetable for the event."
 echo "[*] Enter '3' to view the rules and guidelines for the event."
+echo ""
 }
+read -p "Option: " mainChoice
+
+case $mainChoice in
+
+1) clear; f_option1;;
+2) clear; f_option2;;
+3) clear; f_option3;;
