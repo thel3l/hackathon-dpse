cad4999befa641dd1c5f347ed522c02aa1944b19
rithvikvibhu <rithvikvibhu@gmail.com>
2015-10-04 13:47:07 +0000

    Bare working sub

--- sub.php
+++ sub.php
@@ -1,10 +1,12 @@
 <?php
 
 $name = $_POST["name"];
+//$name = "nammmmee";                       DEBUG VARIABLE
 $email = $_POST["email"];
+//$email = "emmmmaaiiill";                  DEBUG VARIABLE
 $ip = $_SERVER['REMOTE_ADDR'];
 $date = date("d-m-Y");
-echo "$name, $email, $ip, $date";
+//echo "$name, $email, $ip, $date";
 
 $servername = getenv('IP');
 $username = getenv('C9_USER');
@@ -16,10 +18,14 @@ $dbport = 3306;
 // Create connection
 $db = new mysqli($servername, $username, $password, $database, $dbport);
 
+$query = "INSERT INTO `subs` (`Name`, `Email`, `Date`, `IP`) VALUES ('".$name."','".$email."','".$date."','".$ip."')";
 
-if(!$result = $db->query("INSERT INTO `subs`(`Name`, `Email`, `Date`, `IP`) VALUES ($name,$email,$date,$ip)")){
+echo "query: ".$query."<br>";
+
+if(!$result = $db->query($query)){
     die('There was an error running the query [' . $db->error . ']');
 }
+
 var_dump($result);
 
 
