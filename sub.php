b94f46943d305dfe3cdf614408b8f687ba761fd9
rithvikvibhu <rithvikvibhu@gmail.com>
2015-10-04 14:07:33 +0000

    Validate fields and refined code

--- sub.php
+++ sub.php
@@ -1,12 +1,26 @@
 <?php
 
 $name = $_POST["name"];
-//$name = "nammmmee";                       DEBUG VARIABLE
+echo $name;
+//$name = "nammmmee";                         // DEBUG VARIABLE
 $email = $_POST["email"];
-//$email = "emmmmaaiiill";                  DEBUG VARIABLE
+//$email = "emmmmaaiiill";                    // DEBUG VARIABLE
 $ip = $_SERVER['REMOTE_ADDR'];
-$date = date("d-m-Y");
-//echo "$name, $email, $ip, $date";
+//echo "$name, $email, $ip";                // DEBUG VARIABLE
+
+if($name == "" || $email == "" || $name == NULL || $email == NULL) {
+    die("Fill up your name and your email");
+}
+
+//Validate name
+if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+    die("Only letters and white space allowed for name");
+}
+
+//Validate email
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    die("Invalid email format");
+}
 
 $servername = getenv('IP');
 $username = getenv('C9_USER');
@@ -14,19 +28,16 @@ $password = "";
 $database = "dpse";
 $dbport = 3306;
 
-
-// Create connection
 $db = new mysqli($servername, $username, $password, $database, $dbport);
+$query = "INSERT INTO `subs` (`Name`, `Email`, `Date`, `IP`) VALUES ('".$name."','".$email."',NOW(),'".$ip."')";
 
-$query = "INSERT INTO `subs` (`Name`, `Email`, `Date`, `IP`) VALUES ('".$name."','".$email."','".$date."','".$ip."')";
-
-echo "query: ".$query."<br>";
+//echo "query: ".$query."<br>";             // DEBUG VARIABLE
 
 if(!$result = $db->query($query)){
     die('There was an error running the query [' . $db->error . ']');
 }
 
-var_dump($result);
+var_dump($result);                        // DEBUG VARIABLE
 
 
 ?>
