8b1b0bb2bfeba31940bb5aeee1c8255cbb8580b8
thel3l <i.rithwik@protonmail.com>
2015-10-04 22:05:02 +0530

    added email verification check

--- sub.php
+++ sub.php
@@ -33,6 +33,16 @@ $query = "INSERT INTO `subs` (`Name`, `Email`, `Date`, `IP`) VALUES ('".$name."'
 
 //echo "query: ".$query."<br>";             // DEBUG VARIABLE
 
+
+$name = mysql_escape_string($_POST['name']);
+$email = mysql_escape_string($_POST['email']);
+
+
+if(!eregi("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$", $email)){
+  // Return Error - Invalid Email
+}else{
+  // Return Success - Valid Email
+}
 if(!$result = $db->query($query)){
     die('There was an error running the query [' . $db->error . ']');
 }
