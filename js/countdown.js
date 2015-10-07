02519e4234aceac02bf60f9d8bc2743cf5e393da
lunaroyster <oneinaquadrillion@live.com>
2015-10-07 20:57:51 +0530

    Cleaned the markup of all javascript

--- js/countdown.js
+++ js/countdown.js
@@ -2,11 +2,18 @@ $(function() {
     /*$('.countdown').countdown({
         date: "November 7, 2015 07:00:00"
     });*/
-
     $('.countdown.styled').countdown({
-          date: "November 7, 2015 07:00:00",
-          render: function(data) {
-            $(this.el).html("<div class=\"strongclass\">" + this.leadingZeros(data.days, 2) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
-          }
-        });
+        date: "November 7, 2015 07:00:00",
+        render: function(data) {
+            $(this.el).html("<div class=\"strongclass\">" +
+                this.leadingZeros(data.days, 2) +
+                " <span>days</span></div><div>" + this.leadingZeros(
+                    data.hours, 2) +
+                " <span>hrs</span></div><div>" + this.leadingZeros(
+                    data.min, 2) +
+                " <span>min</span></div><div>" + this.leadingZeros(
+                    data.sec, 2) +
+                " <span>sec</span></div>");
+        }
+    });
 });
