e88b103a6b514f044774ed29e7d12422eab647bf
root <i.rithwik@protonmail.com>
2015-10-12 14:35:10 +0530

    Revert "butchered 1337"
    This reverts commit e92cc58705dfa49c4ed5dc5f37e13a45e103527e.

---
+++ 1337toggle/js/countdown.js
@@ -0,0 +1,19 @@
+$(function() {
+    /*$('.countdown').countdown({
+        date: "November 7, 2015 07:00:00"
+    });*/
+    $('.countdown.styled').countdown({
+        date: "November 7, 2015 07:00:00",
+        render: function(data) {
+            $(this.el).html("<div class=\"strongcountdown\">" +
+                this.leadingZeros(data.days, 2) +
+                " <span>days</span></div><div class=\"strongcountdown\">" + this.leadingZeros(
+                    data.hours, 2) +
+                " <span>hrs</span></div><div>" + this.leadingZeros(
+                    data.min, 2) +
+                " <span>min</span></div><div>" + this.leadingZeros(
+                    data.sec, 2) +
+                " <span>sec</span></div>");
+        }
+    });
+});
