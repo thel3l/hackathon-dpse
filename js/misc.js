8d2e937875a76b925cf00fd6490b34cc16dc1138
thel3l <i.rithwik@protonmail.com>
2015-10-17 21:38:33 +0530

    editing the DPSE logo 1/10

--- js/misc.js
+++ js/misc.js
@@ -1,5 +1,5 @@
 $(function() {
-    
+
     //Mode Switch
     $(".switch").change(function() {
         window.location.assign("1337/index.html");
@@ -13,13 +13,13 @@ $(function() {
             //alert(bottonOffset);
             $("html, body").animate( {scrollTop: ( bottonOffset )}, { queue: false, duration: 500, easing: 'swing', complete: function() { $("iframe.map").remove() } });
             //.animate( {scrollTop: ( $("#sect4").offset().top )}, '500', 'swing', );
-            
+
         } else {
             $( "#infocontainer" ).append('<iframe class="map" frameborder="0" height="450" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d52326.308770815405!2d77.69481063320941!3d12.896391034851758!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae12b4e0ca4ad5%3A0xe7851645e27bf86c!2sDelhi+Public+School!5e0!3m2!1sen!2sin!4v1443842307164" style="border:0" width="100%"></iframe>');
             $("html, body").animate( {scrollTop: ( $("iframe.map").offset().top )}, '500', 'swing');
         }
     })
-    
+
 
     //Countdown
     $('.countdown.styled').countdown({
@@ -36,7 +36,7 @@ $(function() {
                 " <span>sec</span></div>");
         }
     });
-    
+
 
     //Smooth scroll
     $('a[href*=#]:not([href=#])').click(function() {
@@ -51,11 +51,11 @@ $(function() {
             }
         }
     });
-    
+
 
     //CSS Fix on load
     $('.nav').css({ 'position': 'fixed', 'top': 0, 'left': 0, 'text-align': 'center'});
-    
+
 
     //Signup bar Toggle with close button
     $(".signup").hide();
