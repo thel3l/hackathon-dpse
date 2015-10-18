ecbb1909f124e4aa9f39cf8d9b5e17a362c07280
thel3l <i.rithwik@protonmail.com>
2015-10-18 18:28:59 +0530

    added a signup.js

---
+++ signup/js/signup.js
@@ -0,0 +1,62 @@
+$(function() {
+
+    // Variable to hold request
+    var request;
+
+    // Bind to the submit event of our form
+    $("#subform").submit(function(event){
+
+        // Abort any pending request
+        if (request) {
+            request.abort();
+        }
+
+        // setup some local variables
+        var $form = $(this);
+
+        // Let's select and cache all the fields
+        var $inputs = $form.find("input");
+
+        // Serialize the data in the form
+        var serializedData = $form.serialize();
+
+        // Let's disable the inputs for the duration of the Ajax request.
+        // Note: we disable elements AFTER the form data has been serialized.
+        // Disabled form elements will not be serialized.
+        $inputs.prop("disabled", true);
+
+        // Fire off the request to /form.php
+        request = $.ajax({
+            url: "https://quadricfusion-rithvikvibhu.c9.io/api/codefest/sub.php",
+            type: "post",
+            data: serializedData
+        });
+
+        // Callback handler that will be called on success
+        request.done(function (response, textStatus, jqXHR){
+            // Log a message to the console
+            console.log("Hooray, it worked!");
+            alert(response);
+        });
+
+        // Callback handler that will be called on failure
+        request.fail(function (jqXHR, textStatus, errorThrown){
+            // Log the error to the console
+            console.error(
+                "The following error occurred: "+
+                textStatus, errorThrown
+            );
+        });
+
+        // Callback handler that will be called regardless
+        // if the request failed or succeeded
+        request.always(function () {
+            // Reenable the inputs
+            $inputs.prop("disabled", false);
+        });
+
+        // Prevent default posting of form
+        event.preventDefault();
+    });
+
+});
